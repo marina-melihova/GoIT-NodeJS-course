694b44cd6dac6e7c3ec0823583b1ec001b015238
Marina-Melikhova <marinamelihova29@gmail.com>
2020-10-28 19:10:54 +0200

    start testing patch

--- tests/updateAvatar.test.js
+++ tests/updateAvatar.test.js
@@ -1,32 +1,81 @@
+require('dotenv').config({ path: './.env' });
+const path = require('path');
+const jwt = require('jsonwebtoken');
+// const { default: context } = require('jest-plugin-context');
 const supertest = require('supertest');
+// const chai = require('chai');
+const expect = require('chai').expect;
+const UserModel = require('../src/api/users/usersModel');
 const crudServer = require('../src/server');
 
 describe('Update avatar Test Suite', () => {
+  const fileName = 'avatar.jpg';
+  const imgFile = path.join(__dirname, `../public/images/${fileName}`);
   let app;
   let response;
 
-  beforeAll(async () => {
-    await crudServer.setup();
-    app = crudServer.app;
-    request = supertest(server);
-
-    response = await request
-      .post('/sign-up')
-      .field('username', 'user1')
-      .field('password', 'qwerty')
-      .attach('avatar', '1603529226139.jpg');
+  before(async () => {
+    const server = await crudServer.setup();
+    app = server.app;
   });
 
-  afterAll(async () => {
-    await fsPromises.unlink(response.body.localPath);
-  });
+  // afterAll(async () => {
+  //   await fsPromises.unlink(response.body.localPath);
+  // });
 
-  it('should return 200', () => {
-    expect(response.status).toEqual(200);
-  });
+  // context('when bad token was provided', () => {
+  //   before(async () => {
+  //     response = await supertest(app)
+  //       .patch('/users/avatar')
+  //       .set('Authorization', '')
+  //       .attach('avatar', imgFile);
+  //   });
 
-  it('should save file', async () => {
-    const localPath = response.body.localPath;
-    await fsPromises.lstat(localPath);
+  it('should return 401 error when bad token was provided', async () => {
+    // expect(response.status).toEqual(401);
+    await supertest(app)
+      .patch('/users/avatar')
+      .set('Authorization', '')
+      .attach('avatar', imgFile)
+      .expect(401);
+  });
+  // });
+
+  context('when good token was provided', () => {
+    let createdUser, token;
+    const newUser = {
+      email: 'testUser@email.com',
+      password: 'qwerty12',
+    };
+
+    before(async () => {
+      response = await supertest(app).post('/auth/register').json(newUser);
+      createdUser = await UserModel.findById(response.body.id);
+      token = jwt.sign({ userId: createdUser.id }, process.env.JWT_SECRET);
+
+      response = await supertest(app)
+        .patch('/users/avatar')
+        .set('Authorization', `Bearer ${token}`)
+        .attach('avatar', imgFile);
+    });
+
+    it('should return 200 successfull response', () => {
+      // expect(response.status).toEqual(200);
+      expect(response.status).to.equal(200);
+    });
+
+    it('should return expected result', () => {
+      // expect(response.body).toEqual({avatarURL: `http://localhost/images${fileName}`});
+      expect(response.body).to.deep.equal({
+        avatarURL: `http://localhost/images${fileName}`,
+      });
+    });
+
+    it("should create new field in user's document", () => {
+      // expect(createdUser.avatarURL).toEqual(`http://localhost/images${fileName}`);
+      expect(createdUser.avatarURL).to.equal(
+        `http://localhost/images${fileName}`,
+      );
+    });
   });
 });

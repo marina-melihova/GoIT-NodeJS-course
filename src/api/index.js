06cda5aeb0690fd7989cf07b03857a4229d6b437
Marina-Melikhova <marinamelihova29@gmail.com>
2020-10-29 22:23:08 +0200

    prepare hw-6

--- src/api/index.js
+++
@@ -1,5 +0,0 @@
-const contactsRouter = require('./contacts/contactsRouter');
-const authRouter = require('./auth/authRouter');
-const usersRouter = require('./users/usersRouter');
-
-module.exports = { contactsRouter, authRouter, usersRouter };
--- src/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { contactsRouter, authRouter, usersRouter } = require('./api');
-
-const app = express();
-app.use('/contacts', contactsRouter);
-app.use('/auth', authRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
